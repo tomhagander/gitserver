f4f734e1941150c6dfb91c2c5d34bf7c393b7453
Tom Hagander <tom.hagander@gmail.com>
2022-04-27 10:43:46 +0200

    work

--- clientserver-main/Project/database.h
+++ clientserver-main/Project/database.h
@@ -6,36 +6,38 @@
 #include <vector> 
 using std::string;
 using std::vector;
+using std::pair;
 
 class database{
     public:
         database();
+
         ~database();
 
         // borde vara vector<pair<int, string>>
-        vector<std::pair<int, string> > list_groups() const;
+        vector<pair<int, string> > list_groups() const;
 
         // should return false if already exists
         bool create_group(string news_group);
 
         // borde nog inte ta string, ska deleta på id nbr
-        bool delete_group(string news_group);
+        bool delete_group(int ng_id_nbr);
 
         // borde vara vector<pair<int, string>>. Borde ta int och inte string. Borde throwa does not exist, finns h-filer för dessa exceptions i include 
-        vector<string> list_articles(string news_group) const;
+        vector<pair<int, string> > list_articles(int ng_id_nbr) const;
 
         // borde ta id nbrs istället för string. Borde throwa does not exsist
         // VAL att göra: Ska server_interface känna till article eller ska den passa referenser till read?? Implementerar referenser sålänge
-        article read(string news_group, string article_name) const;
+        article read(int ng_id_nbr, int art_id_nbr, string& title, string& author, string& text) const;
 
         // borde ta id nbr. Vad vi väljer ovan påverkar vad som skickas in till denna.
         // borde alltså ta int string string string
-        bool write(string news_group, article art);
+        bool write(int ng_id_nbr, string title, string author, string text);
 
         // borde inte ta string, ska vara id nbr. Borde throwa två olika sorters error beroende på vilken som inte finns
-        bool delete_article(string news_group, string article_name);
+        bool delete_article(int ng_id_nbr, int art_id_nbr);
     private:
-        vector<std::pair<string, vector<article> > > groups;
+        vector<std::pair<Newsgroup, vector<article> > > groups;
 };
 
 
